7659bd9024e6f4ffd00d021ecb7891281aafe93c
Relacosm <hraj51399@gmail.com>
2025-09-01 00:46:55 +0530

    Time: 1 ms (22.2%), Space: 18 MB (24.3%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.py
+++ 2058-concatenation-of-array/2058-concatenation-of-array.py
@@ -1,5 +1,7 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        nums.extend(nums.copy())
-        return nums
-        
+        ans=[]
+        for i in range(2):
+            for j in nums:
+                ans.append(j)
+        return ans
